fae0de7dcdf358c6c4833a0c218eb066f190a817
gjuste <gaetanjuste2@gmail.com>
2018-03-28 14:21:37 +0200

    Save

--- anim.js
+++
@@ -1,6 +0,0 @@
-var Animation = (function() {
-	var lien_apropos = $('.page1__lien a:first-of-type'),
-	img_page1 = $('.page1__photo img');
-
-	img_page1.hover(function () {lien_apropos.addClass('hover_lien_apropos');}, function () {lien_apropos.removeClass('hover_lien_apropos')});
-})();
